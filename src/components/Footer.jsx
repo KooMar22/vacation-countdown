94c278eb416b32c68f2755128e24250174522f8e
KooMar <djhracak@gmail.com>
2025-02-17 18:49:27 +0100

    commit - footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="footer">
-      <p><a href="https://github.com/KooMar22?tab=repositories">MarkanDreams</a> ⓒ {year}</p>
+      <p><a href="https://github.com/KooMar22?tab=repositories" target="_blank" rel="noopener noreferrer">MarkanDreams</a> ⓒ {year}</p>
     </footer>
   );
 }
